72e567d08c51d32c183ee79a404e41c02bd2ecb9
OTakumi <takumi.o.0410@gmail.com>
2026-08-12 16:48:52 +0900

    Implement tests for the OR operation

--- crates/engine/src/ast.rs
+++ crates/engine/src/ast.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum AST {
     Char(char),
     Plus(Box<AST>),
--- crates/engine/src/or.rs
+++ crates/engine/src/or.rs
@@ -12,3 +12,47 @@ pub fn fold_or(mut seq_or: Vec<AST>) -> Option<AST> {
         seq_or.pop()
     }
 }
+
+mod tests {
+    use super::*;
+    use crate::ast::AST;
+
+    #[test]
+    fn test_fold_or_emply() {
+        // 要素が空の場合
+        let input: Vec<AST> = vec![];
+        assert_eq!(fold_or(input), None);
+    }
+
+    #[test]
+    fn test_fold_or_single_element() {
+        // 要素が1個の場合
+        let input: Vec<AST> = vec![AST::Char('a')];
+        let expected = Some(AST::Char('a'));
+
+        assert_eq!(fold_or(input), expected);
+    }
+
+    #[test]
+    fn test_fold_or_two_elements() {
+        // 要素が2個の場合
+        let input: Vec<AST> = vec![AST::Char('a'), AST::Char('b')];
+        let expected = Some(AST::Or(Box::new(AST::Char('a')), Box::new(AST::Char('b'))));
+
+        assert_eq!(fold_or(input), expected);
+    }
+
+    #[test]
+    fn test_fold_or_three_elements_right_associative() {
+        // 要素が3個のケース（右結合になるかを検証）
+        let input = vec![AST::Char('a'), AST::Char('b'), AST::Char('c')];
+
+        // 期待されるツリー構造: a | (b | c)
+        let expected = Some(AST::Or(
+            Box::new(AST::Char('a')),
+            Box::new(AST::Or(Box::new(AST::Char('b')), Box::new(AST::Char('c')))),
+        ));
+
+        assert_eq!(fold_or(input), expected);
+    }
+}
